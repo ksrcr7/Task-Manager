c81c378021b2f81646dcf82682e555196f18febb
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-11-10 16:16:39 +0330

    "feat(task): add Task entity with id/title/done and validation; forbid mutable id"

--- include/Task.h
+++ include/Task.h
@@ -1,6 +1,49 @@
-
-
 #ifndef TASK_MANAGER_TASK_H
 #define TASK_MANAGER_TASK_H
+
+#include <iostream>
+#include <string>
+
+
+class Task{
+private:
+    int id{};
+    bool done{};
+    std::string title;
+    static bool IdValid(int);
+    static bool TitleValid(const std::string&);
+
+public:
+    Task() = default;
+    Task(int,bool,const std::string& );
+    [[nodiscard]] int getId()const;
+    [[nodiscard]] const std::string & getTitle()const;
+    [[nodiscard]] bool isDone()const;
+    void SetTitle(const std::string&);
+    void SetDone(bool);
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //TASK_MANAGER_TASK_H
--- src/lib/Task.cpp
+++ src/lib/Task.cpp
@@ -1,3 +1,60 @@
-//
-// Created by ASUS on 11/10/2025.
-//
+#include "Task.h"
+#include <algorithm>
+
+bool Task::IdValid(int s) {
+    if(s < 1)return false;
+    return true;
+}
+
+bool Task::TitleValid(const std::string & t) {
+    if(t.empty())return false;
+    else if(std::all_of(t.begin(),t.end(),
+                        [](unsigned char c){return std::isspace(c);}))
+        return false;
+
+    return true;
+}
+
+int Task::getId() const {
+    return id;
+}
+
+const std::string &Task::getTitle() const {
+    return title;
+}
+
+bool Task::isDone() const {
+    return done;
+}
+
+Task::Task(int n, bool d, const std::string& t) {
+    if(!IdValid(n)){
+        throw std::invalid_argument("Id must be >= 1.");
+    }
+    if(!TitleValid(t)){
+        throw std::invalid_argument("Title must not be empty.");
+    }
+
+    id = n;
+    done = d;
+    title = t;
+
+
+
+
+}
+
+void Task::SetTitle(const std::string &t) {
+    if(!TitleValid(t)){
+        throw std::invalid_argument("Title must not be empty.");
+    }
+    title = t;
+}
+
+void Task::SetDone(bool d) {
+    done = d;
+
+}
+
+
+
